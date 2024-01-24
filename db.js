4988ebee02cccce79807693c8c6530930930640a
Dennis Muturi <denismutur@gmail.com>
2024-01-24 12:50:50 +0300

    Add .gitignore and package.json files, and implement server.js with necessary routes and middleware.

---
+++ db.js
@@ -0,0 +1,134 @@
+const dotenv=require('dotenv');
+dotenv.config();
+const bcrypt = require('bcrypt');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const client = new MongoClient(process.env.CONNECTIONSTRING, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function connectToDb() {
+    await client.connect(); 
+}
+
+async function closeDb() {
+    await client.close();
+}
+
+async function changePassword({userid,password}) {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    connectToDb();
+    const objectifiedId=new ObjectId(userid);
+    const hash = await bcrypt.hash(password,10);
+    // Store hash in your password DB.
+    const results = await client.db("chatApp").collection('userData').updateOne({_id:objectifiedId},{$set:{password:hash}});
+    console.log(results);
+    return results;  
+  }  catch (error) {
+    console.error('Error in ChangePassword:', error);
+  }
+}
+async function insertUserData({email,username,password}) {
+  try {
+    // Connect the client to the server (optional starting in v4.7)
+    connectToDb();
+    const hash = await bcrypt.hash(password,10);
+    // Store hash in your password DB.
+    const results = await client.db("chatApp").collection('userData').insertOne({email:email,username:username,password:hash});
+    return results.insertedId;
+  }  catch (error) {
+    console.error('Error in insertUserUserData:', error);
+  }
+}
+async function authenticateUser({email,password}) {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    connectToDb();
+    const dbresults=await client.db("chatApp").collection('userData').find({email:email}).toArray();
+    if(dbresults.length){
+        const match = await bcrypt.compare(password, dbresults[0].password);
+        if(match){
+            console.log('successful login');
+            //console.log(dbresults);
+            return {results:dbresults,status:'success'};
+        }
+        else{
+            console.log('wrong password')
+            return 'wrong password'
+        }
+    }else{
+        console.log('invalid email');  
+        return 'invalid email'
+    }
+    
+  }  catch (error) {
+    console.error('Error in authenticateUser:', error);
+  }
+}
+//insertUserData().catch(console.dir);
+async function getLiveMessages(socket,userid){
+    connectToDb();
+    const collection=client.db('chatApp').collection('messages');
+    const objectifiedId=new ObjectId(userid);
+    const changeStream = collection.watch([{$match:{'operationType':'insert','fullDocument.receiverid':objectifiedId}}]);
+    changeStream.on('change', (next) => {
+      socket.emit('db-changes',{message:next.fullDocument.message,senderid:next.fullDocument.senderid});
+      console.log(next);
+    });
+    return changeStream;
+}
+async function getMessages(userid){
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    connectToDb();
+    const objectifiedId=new ObjectId(userid);
+    const results=await client.db("chatApp").collection('messages').find({ $or: [ { senderid:objectifiedId }, { receiverid: objectifiedId} ]}).toArray();
+    //console.log(results);
+    return results;
+    
+  }  catch (error) {
+    console.error('Error in getMessages:', error);
+  }
+
+}
+async function getUsers(){
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    connectToDb();
+    //const results=await client.db("chatApp").collection('userData').find().toArray();
+    const results = await client.db("chatApp").collection('userData').find({}, { projection: { _id: 1, username: 1 } }).toArray();
+    //console.log(results);
+    return results;
+    
+  }  catch (error) {
+    console.error('Error in getusers:', error);
+  }
+
+}
+async function insertMessages({userid,message,receiverid}) {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    connectToDb();
+    const objectifiedId=new ObjectId(userid);
+    const objectifiedReceiverId=new ObjectId(receiverid);
+    const results=await client.db("chatApp").collection('messages').insertOne({senderid:objectifiedId,message:message,receiverid:objectifiedReceiverId,time:new Date()});
+    console.log(results); 
+  }  catch (error) {
+    console.error('Error in insertMessages:', error);
+  }
+}
+//getUsers();
+//insertUserData({username:'Uhuru',password:'Kenyatta'});
+//rambo();
+//getMessages('dennis');
+//insertMessages({userid:'65a6a534c9ba55595a231a61',message:'agwambo'})
+//changePassword({userid:'65a6a534c9ba55595a231a61',password:'BrownOwino'})
+//authenticateUser({username:'BabuOwin',password:'BrownOwino'})
+//insertMessages({userid:'65a6a534c9ba55595a231a61',message:'sasa Owino'})
+//rambo(null,'65a6a534c9ba55595a231a61'); 
+
+module.exports={insertUserData,changePassword,authenticateUser,getLiveMessages,getMessages,insertMessages,getUsers}
+
---
+++ server.js
@@ -0,0 +1,140 @@
+const {insertUserData,changePassword,authenticateUser, getLiveMessages,getMessages,insertMessages,getUsers}=require('./db.js');
+const io=require('socket.io')(3000,{cors:{origin:'http://localhost:5173'}});
+const express=require('express');
+const app=express();
+const cors=require('cors');
+const jwt=require('jsonwebtoken');
+const dotenv=require('dotenv');
+const cookieParser=require('cookie-parser');
+const corsOptions = {
+  origin: 'http://localhost:5173', // replace with the origin of your client
+  credentials: true, // this allows the session cookie to be sent with the request
+};
+dotenv.config();
+io.use((socket,next)=>{
+    if(socket.handshake.auth.token){
+        socket.username=socket.handshake.auth.token;
+        jwt.verify(socket.handshake.auth.token,process.env.JWT_SECRET,(err,decoded)=>{
+            if(err){
+               next(new Error('wrong token'));
+            }else{
+                socket.userid=decoded.userid;
+                //console.log(socket.userid);
+                next();
+            }
+        })    
+    }
+    else{
+        next(new Error('please send token'));
+    }
+})
+io.on('connection',async (socket)=>{
+    console.log(socket.id);
+    //console.log(socket.userid)
+    const changeStream=await getLiveMessages(socket,socket.userid);
+    socket.on('disconnect',()=>{
+        console.log('goodbye');
+        changeStream.close();
+    })
+})
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors(corsOptions));
+function verifyUser(req,res,next){
+    if(req.cookies){
+        jwt.verify(req.cookies.token,process.env.JWT_SECRET,(err,decoded)=>{
+            if(err){
+                return res.json({status:'wrong password'})
+            }else{
+                req.userid=decoded.userid;
+                //console.log(req.userid);
+            }
+        })  
+        next()
+    }
+    else{
+        return res.json({status:'no token given'});
+    }
+}
+app.get('/messages',verifyUser,async (req,res)=>{
+    const messages=await getMessages(req.userid);
+    res.json(messages);
+})
+app.post('/sendmessage',verifyUser,async (req,res)=>{
+    insertMessages({userid:req.userid,message:req.body.message,receiverid:req.body.receiverid});
+    res.json({status:'suceessfully added message'});
+})
+app.post('/register',(req,res)=>{
+        insertUserData(req.body).then((userId)=>{
+        if(userId){
+            //console.log(userId);
+            res.cookie('token',jwt.sign({userid:userId},process.env.JWT_SECRET));
+            res.json({status:'successfully registered'});
+        }
+        else{
+        res.json({status:'failure register again'});
+        }
+
+    }) 
+})
+app.get('/userfriends',verifyUser,async (req,res)=>{
+    //console.log('users')
+    const users=await getUsers();
+    res.json(users);
+})
+app.post('/changepassword',verifyUser,async (req,res)=>{
+    const authenticateUserperson=await authenticateUser(req.body);
+    if(authenticateUserperson.status=='success'){
+        const changeuserPassword=await changePassword({userid:req.userid,password:req.body.newpassword});
+        if(changeuserPassword){
+            res.json({status:'change Password successfully'})
+        }
+        else{
+            res.json({status:'failed to change password'});
+        }
+    }
+    else{
+        res.json({status:'wrong initial password'});
+    }   
+})
+app.post('/login',async (req,res)=>{
+    const authenticateUserperson=await authenticateUser(req.body)
+    if(authenticateUserperson.status=='success'){
+        const userId=authenticateUserperson.results[0]._id;
+        //console.log(userId);
+        //insertMessages({userid:userId,message:'ODM Baba'})
+        res.cookie('token',jwt.sign({userid:userId},process.env.JWT_SECRET))
+        res.json({status:'success',username:authenticateUserperson.results[0].username});
+    }
+    else if(authenticateUserperson=='wrong password'){
+        res.json({status:'wrong password'})
+    }
+    else if(authenticateUserperson=='invalid email'){
+        res.json({status:'invalid email'});
+    }
+    else{
+        res.send('failure');
+    }
+})
+app.listen(4000);
+/*io.on('connection',socket=>{
+    console.log(socket.id);
+    socket.on('custom-event',(postmanMessage,room)=>{
+        console.log(postmanMessage);
+        if(room==''){
+            socket.broadcast.emit('bro-code','thank you')
+
+        }
+        else{
+            socket.to(room).emit('bro-code','thank you');
+        }
+        
+    })
+    socket.on('join-room',(room)=>{
+        socket.join(room);
+    })
+})*/
+
+
+
